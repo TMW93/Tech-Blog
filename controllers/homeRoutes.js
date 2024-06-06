9b319ee1257f5c96411683839b286d193fc1b75d
TimWong <timwong.tw93@gmail.com>
2024-06-06 23:29:57 +1000

    added post model and dashboard now loads, but havent implemented creating posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,6 @@
 const router = require(`express`).Router();
 const {User} = require(`../models`);
+const {Post} = require(`../models`);
 const withAuth = require(`../utils/auth`);
 
 router.get(`/`, withAuth, async (req, res) => {
@@ -9,9 +10,30 @@ router.get(`/`, withAuth, async (req, res) => {
       order: [['name', 'ASC']],
     });
 
-    const users = userData.map((data) => data.get({ plain: true }));
+    const users = userData.map((data) => data.get({plain: true}));
 
     res.render('homepage', {users , logged_in: req.session.logged_in, logged_in_username: req.session.username});
+
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get(`/dashboard`, withAuth, async (req, res) => {
+  try {
+    const myUser = await User.findOne({
+      where: {name: req.session.username}
+    });
+
+    const userID = myUser.id;
+
+    const postData = await Post.findAll({
+      where: {user_id: userID,},
+    }); 
+    
+    const posts = postData.map((data) => data.get({plain: true}));
+
+    res.render('dashboard', {posts, logged_in: req.session.logged_in, logged_in_username: req.session.username});
 
   } catch (error) {
     res.status(500).json(error);
