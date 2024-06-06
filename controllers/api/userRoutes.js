1f529feae9f0f775b35f90b32a3dee809ff65d5e
TimWong <timwong.tw93@gmail.com>
2024-06-06 15:21:52 +1000

    users can now logout

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -31,4 +31,14 @@ router.post(`/login`, async (req, res) => {
 
 });
 
+router.post(`/logout`, (req, res) => {
+  if(req.session.logged_in) {
+    req.session.destroy( () => {
+      res.status(200).end();
+    });
+  } else {
+    res.status(400).end();
+  }
+});
+
 module.exports = router;
