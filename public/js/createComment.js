f1a2fcfa7a8ff7b7b86c0b4bf08f1cb022109eba
TimWong <timwong.tw93@gmail.com>
2024-06-10 22:47:17 +1000

    added an update button that only shows if logged in user owns post

--- public/js/createComment.js
+++ public/js/createComment.js
@@ -25,3 +25,21 @@ const commentFormHandler = async (event) => {
 };
 
 document.querySelector(`.addcomment-form`).addEventListener(`submit`, commentFormHandler);
+
+const buttonsDiv = document.getElementById(`buttons-div`);
+const updateButton = document.getElementById(`update-button`);
+const updateFormCard = document.getElementById(`updatepost-card`);
+
+const buttonsDivCheck = buttonsDiv.getAttribute(`data-id`);
+const updateButtonCheck = updateButton.getAttribute(`data-id`);
+
+const updateButtonHandler = async () => {
+  updateFormCard.classList.remove(`d-none`);
+};
+
+if(buttonsDivCheck === updateButtonCheck) {
+  buttonsDiv.classList.remove(`d-none`);
+  updateButton.classList.remove(`d-none`);
+}
+
+document.getElementById(`update-button`).addEventListener(`click`, updateButtonHandler);
